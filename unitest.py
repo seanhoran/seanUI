95a2c35036abb802df022d76da27a14b60c83b48
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-15 21:35:43 -0500

    Update unitest.py

--- unitest.py
+++ unitest.py
@@ -58,7 +58,7 @@ st.pyplot(fig)
 st.write("Kriged Grade")
 
 fig, ax = plt.subplots()
-ax.scatter(xc,yc,c=zc, marker="s", s=3)
+ax.scatter(xc,yc,c=zc, marker="s", s=3, alpha=0.5, linewidth=0.5)
 ax.scatter(x,y,c=z, edgecolor="black")
 st.pyplot(fig)
 
